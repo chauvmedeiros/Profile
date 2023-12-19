597f4ce70689e4f1e2154aaf458f955db8348d44
chauvmedeiros <chauannytoryy@gmail.com>
2023-12-19 16:51:02 -0300

    add script

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function toggleMode() {
     const html = document.documentElement
-
-   // if(html.classList.contains('light')) {
-   // html.classList.remove('light')
-
-       // } else {
-      //      html.classList.add('ligth')
-      //  }
-
-    html.classList.toggle('light')
-
-}
+    html.classList.toggle("light")
+  
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+  
+    // substituir a imagem
+    if (html.classList.contains("light")) {
+      // se tiver light mode, adicionar a imagem light
+      img.setAttribute("src", "./avatar-bg.png")
+    } else {
+      // set tiver sem light mode, manter a imagem normal
+      img.setAttribute("src", "./Avatar.png")
+    }
+  }
